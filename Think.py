279417ff5d1183a9e51001023aa82747513f9f35
lamour <mylamour@163.com>
2015-08-03 13:54:14 +0800

    基本智能构架，代填充

---
+++ Think.py
@@ -0,0 +1,26 @@
+from Agent import *
+from Action import *
+
+class Think(object):
+    def __init__(self):
+        pass
+    def WhereToLook(self):
+        pass
+    def WhereToWalk(self):
+        pass
+    def WhereToRun(self):
+        pass
+    def WhereToKick(self):
+        pass
+    def HowToLook(self):
+        pass
+    def HowToWalk(self):
+        pass
+    def HowToRun(self):
+        pass
+    def HowToKick(self):
+        pass
+    def HowToAttack(self):
+        pass
+    def HowToDefend(self):
+        pass
